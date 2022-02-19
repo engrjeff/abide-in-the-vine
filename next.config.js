9a790ca6d9887c99bbc49a8038a3c9fa53d5274e
engrjeff <engineerjeff@gmail.com>
2022-02-19 11:23:01 +0800

    site-map test

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
   env: {
     API_PROD_URL: "https://abide-in-the-vine.herokuapp.com",
     API_DEV_URL: "http://localhost:1337",
+    SITE_URL: "http://abideinthevineph.org",
   },
 };
 
