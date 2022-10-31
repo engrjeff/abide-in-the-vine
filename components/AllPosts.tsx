bcc203f3f64b8443c3a95d9e161eb4fa3f0969c3
Jeffrey Segovia <jeffrey.segovia@opsolutions.biz>
2022-10-31 23:37:28 +0800

    migrated the posts to mdx

--- components/AllPosts.tsx
+++ components/AllPosts.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { matchSorter } from "match-sorter";
-import { ArrowRightIcon } from "@heroicons/react/solid";
-import _ from "lodash";
+import React, { useState } from 'react';
+import { matchSorter } from 'match-sorter';
+import { ArrowRightIcon } from '@heroicons/react/solid';
+import _ from 'lodash';
 
-import AppButton from "./AppButton";
-import PostCard from "./PostCard";
-import SearchInput from "./SearchInput";
-import { Post, Tag } from "@utils/types";
-import BlogsStatAndTimeFilter from "./BlogsStatAndTimeFilter";
-import BlogsTagsFilter from "./BlogsTagsFilter";
+import AppButton from './AppButton';
+import PostCard from './PostCard';
+import SearchInput from './SearchInput';
+import { Post, Tag } from '@utils/types';
+import BlogsStatAndTimeFilter from './BlogsStatAndTimeFilter';
+import BlogsTagsFilter from './BlogsTagsFilter';
 
 interface AllPostsProps {
   posts: Post[];
@@ -17,13 +17,13 @@ interface AllPostsProps {
 
 const AllPosts = (props: AllPostsProps) => {
   const { posts, tags } = props;
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState('');
   const [tagFilters, setTagFilters] = useState<string[]>([]);
-  const [selectedFilter, setSelectedFilter] = useState("Most Recent");
+  const [selectedFilter, setSelectedFilter] = useState('Most Recent');
   const [limit, setLimit] = useState(3);
 
   let filteredPosts = matchSorter(posts, search, {
-    keys: ["title"],
+    keys: ['title'],
   });
 
   if (tagFilters.length > 0) {
@@ -34,32 +34,25 @@ const AllPosts = (props: AllPostsProps) => {
 
   const resetFilters = () => {
     setTagFilters([]);
-    setSearch("");
+    setSearch('');
   };
 
-  const sortDirection = selectedFilter === "Most Recent" ? "desc" : "asc";
-  const sortedPosts = _.orderBy(
-    filteredPosts,
-    ["createdAt"],
-    [sortDirection]
-  ).slice(0, limit);
+  const sortDirection = selectedFilter === 'Most Recent' ? 'desc' : 'asc';
+  const sortedPosts = _.orderBy(filteredPosts, ['createdAt'], [sortDirection]).slice(0, limit);
 
   return (
     <div>
       <div className='flex items-center mb-6'>
-        <BlogsStatAndTimeFilter
-          selectedFilter={selectedFilter}
-          onChange={setSelectedFilter}
-        />
+        <BlogsStatAndTimeFilter selectedFilter={selectedFilter} onChange={setSelectedFilter} />
         <div className='ml-none w-full md:w-auto md:ml-auto'>
           <SearchInput search={search} onChange={setSearch} />
         </div>
       </div>
-      <BlogsTagsFilter
+      {/* <BlogsTagsFilter
         tags={tags}
         currentTags={tagFilters}
         onChange={setTagFilters}
-      />
+      /> */}
       <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mt-8'>
         {sortedPosts.length > 0 ? (
           sortedPosts.map((post: any) => <PostCard key={post.id} post={post} />)
